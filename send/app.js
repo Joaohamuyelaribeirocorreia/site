dcb2890ac23f62afe38e0491cb108dfa069b539f
Joaohamuyelaribeirocorreia <htccorreia@gmail.com>
2024-10-21 01:13:32 +0100

    app.js

--- send/app.js
+++ send/app.js
@@ -126,7 +126,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
